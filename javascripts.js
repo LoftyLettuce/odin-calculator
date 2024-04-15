3722d9f3ccc216adb8af65b4ed1d69db1cd34dbb
LoftyLettuce <lubolulubo2@gmail.com>
2024-04-15 16:36:06 +0700

    Make, polish buttons

--- javascripts.js
+++ javascripts.js
@@ -1,11 +1,20 @@
-for (let i = 0; i < 4; i++)
+for (let i = 0, number = 9, lastRow = ['.', 0, 'x']; i < 4; i++)
 {
   let u = document.querySelector('.numbers');
   let row = u.appendChild(document.createElement('div'));
   row.className = "row";
-  for (let u = 0; u < 4; u++)
+  for (let u = 0; u < 3; u++)
   {
-    let key = row.appendChild(document.createElement('div'));
+    let key = row.appendChild(document.createElement('button'));
     key.className = "key";
+    if (i < 3)
+    {
+      key.textContent = number--;
+    }
+    else
+    {
+      key.textContent = lastRow[lastRow.length - 1];
+      lastRow.pop();
+    }
   }
 }
