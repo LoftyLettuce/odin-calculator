d81c2499924acc903c9585fddda7aa2ce5e240ab
LoftyLettuce <lubolulubo2@gmail.com>
2024-04-16 20:42:38 +0700

    Display input

--- javascripts.js
+++ javascripts.js
@@ -1,3 +1,4 @@
+//button for numbers
 for (let i = 0, number = 9, lastRow = ['.', 0, 'x']; i < 4; i++)
 {
   let u = document.querySelector('.numbers');
@@ -18,3 +19,32 @@ for (let i = 0, number = 9, lastRow = ['.', 0, 'x']; i < 4; i++)
     }
   }
 }
+//buttons for operators
+for (let i = 0, operators = ["=", "x", "/", "+", "-"], numberButton = 2; i < 3; i++)
+{
+  let u = document.querySelector('.operators');
+  let row = u.appendChild(document.createElement('div'));
+  row.className = "row";
+  if (i == 2)
+  {
+    numberButton--;
+  }
+  for (let u = 0; u < numberButton; u++)
+  {
+    let key = row.appendChild(document.createElement('button'));
+    key.className = "key";
+    key.textContent = operators[operators.length - 1];
+    operators.pop();
+  }
+}
+//input
+let input = document.querySelector(".expressions");
+let keyboard = document.querySelector(".keyboard");
+keyboard.addEventListener('click', (event) => 
+{
+  if (event.target.className == 'key')
+  {
+    input.value += event.target.textContent;
+  }
+}
+);
